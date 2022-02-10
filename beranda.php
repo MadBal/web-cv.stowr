5c8965fcd80080886e4c2597f1b28b28f15ec55e
MuhammadIqbal <madiqbal5000@gmail.com>
2022-02-10 11:21:19 +0800

    hapus

--- beranda.php
+++ beranda.php
@@ -6,8 +6,6 @@
                         <div class="section-title">Welcome to </div>
                         <h1 class="h1-large">CV.STOWR</h1>
                         <p class="p-large">CV.STWOR didirikan pada tahun 2018 tepatnya pada tanggal 14 agustus tahun 2018 berkedudukan di kota makassar,  dimana pendirinya terdiri empat orang yaitu Dr.Satria Gunawan Zain,S.Pd.,M.T. , Suhartono,S.Kom.,M.Kom, Abdurahman Patta,S.Kom.,M.T, dan Muhammad Riska Babo,S.Pd.,M.Pd.</p>
-                        <a class="btn-solid-lg" href="#services">Offered services</a>
-                        <a class="quote" href="#contact"><i class="fas fa-paper-plane"></i>Get quote</a>
                     </div> <!-- end of text-container -->
                 </div> <!-- end of col -->
                 <div class="col-lg-6 col-xl-7">
