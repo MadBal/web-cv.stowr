b43dc427750382e036f26b5e6e48bf5db1f2825c
MuhammadIqbal <madiqbal5000@gmail.com>
2022-02-15 23:09:52 +0800

    tambah folder dan file admin

--- admin/index.php
+++ admin/index.php
@@ -1,34 +1,98 @@
 <?php
-session_start();
-
-if( !isset($_SESSION["login"]) ) {
-    header("Location: login.php");
-    exit;
-}
-
-require '../koneksi.php';
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Admin</title>
-</head>
-<body>
-    <div align="center">
-      <h2>Halaman Admin</h2>
-      <table>
+<html>
+  <head>
+    <title>CRUD Produk dengan gambar - Gilacoding</title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: #265797;
+      }
+    table {
+      border: solid 1px #DDEEEE;
+      border-collapse: collapse;
+      border-spacing: 0;
+      width: 70%;
+      margin: 10px auto 10px auto;
+    }
+    table thead th {
+        background-color: #DDEFEF;
+        border: solid 1px #DDEEEE;
+        color: #265797;
+        padding: 10px;
+        text-align: left;
+        text-shadow: 1px 1px 1px #fff;
+        text-decoration: none;
+    }
+    table tbody td {
+        border: solid 1px #DDEEEE;
+        color: #333;
+        padding: 10px;
+        text-shadow: 1px 1px 1px #fff;
+    }
+    a {
+          background-color: #265797;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+    }
+    </style>
+  </head>
+  <body>
+    <center><h1>Data Proyek</h1><center>
+    <center><a href="tambah_produk.php">+ &nbsp; Tambah Proyek</a><center>
+    <br/>
+    <table>
+      <thead>
         <tr>
-          <td><a href="?page=visimisi">Input visi misi</a></td>
+          <th>No</th>
+          <th>Judul</th>
+          <th>Dekripsi</th>
+          <th>Gambar</th>
+          <th>Action</th>
         </tr>
-        <tr>
-        <td><a href="logout.php">Logout</a></td>
-        </tr>
-      </table>
-      <?php require "content.php"; ?>
-    </div>
-</body>
+    </thead>
+    <tbody>
+      <?php
+      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+      $query = "SELECT * FROM produk ORDER BY id ASC";
+      $result = mysqli_query($koneksi, $query);
+      //mengecek apakah ada error ketika menjalankan query
+      if(!$result){
+        die ("Query Error: ".mysqli_errno($koneksi).
+           " - ".mysqli_error($koneksi));
+      }
+
+      //buat perulangan untuk element tabel dari data mahasiswa
+      $no = 1; //variabel untuk membuat nomor urut
+      // hasil query akan disimpan dalam variabel $data dalam bentuk array
+      // kemudian dicetak dengan perulangan while
+      while($row = mysqli_fetch_assoc($result))
+      {
+      ?>
+       <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $row['nama_produk']; ?></td>
+          <td><?php echo substr($row['deskripsi'], 0, 20); ?>...</td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;"></td>
+          <td>
+              <a href="edit_produk.php?id=<?php echo $row['id']; ?>">Edit</a> |
+              <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+          </td>
+      </tr>
+         
+      <?php
+        $no++; //untuk nomor urut terus bertambah 1
+      }
+      ?>
+    </tbody>
+    </table>
+  </body>
 </html>
